086d973ff78b404e160f2ed892ca8584d1a763b8
hangyuCho <chg3376@gmail.com>
2022-06-04 20:35:11 +0900

    에제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,28 +1,48 @@
 package study.datajpa.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    // 왜 프로텍티드?
-    // jpa proxy 에서 이용이 가능해짐
-    protected Member() {
-    }
+    // 모든 연관관계는 기본적으로 지연(Lazy)로 설정해야함
+    // 실무에서 성능 관련으로 Lazy가 아니라면 원하는 성능을 뽑아내는데
+    // 큰 어려움이 있음..
+
+    // 지연 로딩이란?
+    // 멤버의 값만 취득하고 연결된 Team은 실제로 값을 확인할 때 취득하는 기법
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMember().add(this);
     }
 
 }
